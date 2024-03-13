0ed488a59ec730d9a17b4fdc6df8a11bed6dca2a
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-03-13 13:52:18 -0700

    added css to footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { Shop } from './Pages/Shop';
 import { LoginSignup } from './Pages/LoginSignup';
+import { Footer } from './Components/Footer/Footer';
 function App() {
   return (
     <div >
@@ -23,6 +24,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 import footer_logo from '../Assets/logo_big.png';
 import instagram_icon from '../Assets/instagram_icon.png';
-import pinterest_icon from '../Assets/pinterest_icon.png';
+import pinterest_icon from '../Assets/pintester_icon.png';
 import whatsapp_icon from '../Assets/whatsapp_icon.png';
 
 export const Footer = () => {
