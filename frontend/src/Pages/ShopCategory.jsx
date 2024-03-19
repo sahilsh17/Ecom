e61aa5e49a05e47d76c3916de4392c5bd0d5d0b5
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-03-19 16:05:51 -0700

    added changes to ShopCategory

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 
 
 export const ShopCategory = (props) => {
@@ -8,6 +9,14 @@ export const ShopCategory = (props) => {
   return (
     <div className="shop-category">
       <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
