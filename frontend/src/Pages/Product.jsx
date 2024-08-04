8a82a25a4676951a1018cf643818aec075b5c015
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-08-04 16:18:22 -0700

    working on breadcrum.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import all_product from '../Components/Assets/all_product';
+import { useParams } from 'react-router-dom';
 export const Product = () => {
+  const { productId } = useParams();
+  const product = all_product.find((e) =>
+    e.id == Number(productId))
   return (
-    <div>Product</div>
+    <div>
+
+    </div>
   )
 }
