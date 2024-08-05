011bfb253ecb0927e9ec68a621aec97d41fc3f57
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-08-05 12:08:38 -0700

    created product display component and its css

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import all_product from '../Components/Assets/all_product';
 import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay';
 export const Product = () => {
   const { productId } = useParams();
   const product = all_product.find((e) =>
     e.id == Number(productId))
   return (
     <div>
-
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
