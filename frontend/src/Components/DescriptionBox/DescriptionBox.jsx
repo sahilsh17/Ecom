98e70ac8352f3256931b7f2401423cfc35ddaa54
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-08-05 12:17:08 -0700

    Created Description Box component

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className='descriptionbox-navigator'>
+        <div className='descriptionbox-nav-box'>
+          Description
+        </div>
+        <div className='descriptionbox-nav-box fade'>
+          Reviews (122)
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import all_product from '../Components/Assets/all_product';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
 import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay';
+import { DescriptionBox } from '../Components/DescriptionBox/DescriptionBox';
 export const Product = () => {
   const { productId } = useParams();
   const product = all_product.find((e) =>
@@ -11,6 +12,7 @@ export const Product = () => {
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   )
 }
