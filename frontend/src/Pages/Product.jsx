0ae03fe556457a9570dd9994bf0027e7add1f037
Sahil Sharma <sahil.sharma237333@gmail.com>
2024-08-05 22:11:24 -0700

    added RelatedProducts component and its css

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
 import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay';
 import { DescriptionBox } from '../Components/DescriptionBox/DescriptionBox';
+import { RelatedProducts } from '../Components/RelatedProducts/RelatedProducts';
 export const Product = () => {
   const { productId } = useParams();
   const product = all_product.find((e) =>
@@ -13,6 +14,7 @@ export const Product = () => {
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
